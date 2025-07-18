c34387a9980e76b7102f239259697428736c823e
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2025-07-18 10:12:12 +0530

    if else_if else

---
+++ if_elseif_else.java
@@ -0,0 +1,15 @@
+class demo
+{
+    public static void main(String args[])
+    {
+        int a= 110;
+        int b=150;
+        int c= 13;
+        if(a<b && a<c)
+            System.out.println("a is the smallest number");
+        else if(b<a && b<c)
+            System.out.println("b is the smallest number");
+        else 
+             System.out.println("c is the smallest number");
+    }
+}
