2683f5b5c5dc4eb28b0c50f14313852ed47d904e
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2025-07-18 09:54:25 +0530

    if else statement

---
+++ if_else.java
@@ -0,0 +1,16 @@
+class demo
+{
+    public static void main(String args[])
+    {
+        int a= 0;
+        int b=15;
+        if(a<b)
+        {
+            System.out.println("b is the maximum number");
+            System.out.println("Thankyou");
+        }
+        else
+            System.out.println("a is the maximum number");
+
+    }
+}
