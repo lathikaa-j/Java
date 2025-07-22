8b3ed3de8e4524dfdf9770744954d2559bce9405
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2025-07-22 10:29:38 +0530

    while loop in java

---
+++ loop.java
@@ -0,0 +1,14 @@
+class demo
+{
+    public static void main(String args[])
+    {
+        // tables of 7
+        int i = 1;
+        while (i<=10)
+        {
+            int n= 7*i;
+            System.out.println("7*" + i + "=" + n);
+            i++;
+        }
+    }
+}    
