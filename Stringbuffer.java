022c92a206d19ecbe79ce663143156a6db12d388
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2025-08-14 11:28:32 +0530

    String Buffer - immutable to mutable string

---
+++ Stringbuffer.java
@@ -0,0 +1,9 @@
+class demo
+{
+    public static void main(String args[])
+    {
+        StringBuffer s = new StringBuffer("Sea");
+        s.append(" is beautiful");
+        System.out.println(s);
+    }
+}
