14411e50b39ab9cf6891c80b693242d0cea711fe
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2025-10-26 11:04:55 +0530

    optimized_thread

---
+++ optimized_thread.java
@@ -0,0 +1,60 @@
+class A extends Thread
+{
+    public void run()
+    {
+        for(int i=1; i<=100;i++ )
+        {
+            System.out.println("Hello");
+            try{ 
+                Thread.sleep(10);
+               }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            
+
+        }
+    }
+}
+
+class B extends Thread
+{
+    public void run()
+    {
+        for(int i=1; i<=100;i++ )
+        {
+            System.out.println("Bye");
+            try{ 
+                Thread.sleep(10);
+               }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            
+        }
+    }
+}
+
+
+class Practice
+{
+    public static void main(String args[])
+    {
+        A obj1= new A();
+        B obj2= new B();
+        obj2.setPriority(10);
+
+        obj1.start();
+        try{ 
+                Thread.sleep(5);
+               }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        obj2.start();
+
+    }
+}
