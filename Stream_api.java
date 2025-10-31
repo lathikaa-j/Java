bf1d62478828a1e4dad02369524eb6f6f88f2e1d
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2025-10-31 19:34:11 +0530

    Stream api

---
+++ Stream_api.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Practice
+{
+    public static void main(String args[])
+    {
+        List<Integer> nums = Arrays.asList(1,8,9,4,8,2);
+        nums.forEach(n-> System.out.println(n));
+    }
+}
